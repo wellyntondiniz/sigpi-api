b0c6dfbe405274e0bd78a90fc4a98c546ade371a
Wellynton Diniz <wellyntondiniz@gmail.com>
2025-10-16 20:57:59 -0400

    cors

---
+++ src/main/java/com/sigpi/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.sigpi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig {
+	
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://127.0.0.1:8081");
+            }
+        };
+    }
+
+}
--- src/main/java/com/sigpi/imovel/ImovelRestController.java
+++ src/main/java/com/sigpi/imovel/ImovelRestController.java
@@ -3,6 +3,7 @@ package com.sigpi.imovel;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/imovel")
 public class ImovelRestController {
